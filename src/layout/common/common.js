eda0f83ee32fb5a0338b555f6e18378b72867147
Arthur Simonyan <1simonyanartur1@gmail.com>
2022-01-14 15:22:51 +0300

    add self-load function adding phone mask

--- src/layout/common/common.js
+++ src/layout/common/common.js
@@ -2,4 +2,10 @@ const { test } = require("../blocks/test/test");
 
 document.addEventListener("DOMContentLoaded", function (event) {
 	test();
+
+	(function addPhoneMask() {
+		let telephone = document.querySelectorAll('input[type="tel"]');
+		let im = new Inputmask("+7 (999) 999-99-99");
+		im.mask(telephone);
+	}());
 });
